989575a76ef5d521b8796c2aa30ef79ac2a3b369
JiaHui <1474352803@qq.com>
2021-08-05 17:42:30 +0800

    查询

--- demoafter/src/main/java/com/atjh/demoafter/controller/UserController.java
+++ demoafter/src/main/java/com/atjh/demoafter/controller/UserController.java
@@ -4,9 +4,14 @@ import com.atjh.demoafter.common.Result;
 import com.atjh.demoafter.entity.User;
 import com.atjh.demoafter.mapper.UserMapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.sql.Wrapper;
 
 @RestController
 @RequestMapping("/user")
@@ -19,5 +24,17 @@ public class UserController {
     public Result<?> save(@RequestBody User user){
         userMapper.insert(user);
         return Result.success();
+    }
+
+    @GetMapping
+    public Result<?> findPage(@RequestParam(defaultValue = "1") Integer pageNum,
+                              @RequestParam(defaultValue = "10") Integer pageSize,
+                              @RequestParam(defaultValue = "") String search){
+        LambdaQueryWrapper<User> wrapper = Wrappers.<User>lambdaQuery();
+        if(StringUtils.isNotBlank(search)){
+            wrapper.like(User::getNickName,search);
+        }
+        Page<User> userPage = userMapper.selectPage(new Page<>(pageNum, pageSize), wrapper);
+        return Result.success(userPage);
     }
 }
